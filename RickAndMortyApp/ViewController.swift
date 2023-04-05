4964c6ad8b63d636124b70aa2071335930c713a8
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2023-04-06 02:41:46 +0300

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Ali Çolak on 6.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
